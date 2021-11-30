1e3fef79abd380fa55081a1f0a3334f462b6f1d2
Munetsi Zunguzira <dev@jetweb.onmicrosoft.com>
2021-11-30 16:00:06 +0200

    Using Interfaces to Describe Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
 
   getAlbum ( id: number ): Observable<Album> {
     return this._http.get(this._albumUrl)
-      .map((response: Response) => <Album>response.json());
+      .map((response ) => <Album>response.json());
   }
 
 }
