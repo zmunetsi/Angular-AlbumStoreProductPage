997b8abdade4361160ebe4ccd515b87e641275ee
Munetsi Zunguzira <dev@jetweb.onmicrosoft.com>
2021-12-01 17:16:51 +0200

    Creating Navigation using the Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
+ 
   { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductPageComponent }
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 ];
 
 @NgModule({
